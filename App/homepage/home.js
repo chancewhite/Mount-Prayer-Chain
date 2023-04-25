f473bd13e3c254df8378dfaf85311a0a8cb635b3
chancewhite <90930126+chancewhite@users.noreply.github.com>
2023-04-24 20:09:48 -0400

    Add files via upload

---
+++ App/homepage/home.js
@@ -0,0 +1,55 @@
+        // Import the functions you need from the SDKs you need
+        // Firebase documentation
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+        import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+        import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js"; // this contains auth, the event listner for creating a new user, the event listener for logging a user in, the object for getting the currently signed in user, and the logout event
+        // TODO: Add SDKs for Firebase products that you want to use
+        // https://firebase.google.com/docs/web/setup#available-libraries
+      
+        // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+          apiKey: "AIzaSyBNbTDHQcdxP3ou4IQFYPKHbmnZNJVsSeo",
+          authDomain: "authentication-6ccca.firebaseapp.com",
+          databaseURL: "https://authentication-6ccca-default-rtdb.firebaseio.com",
+          projectId: "authentication-6ccca",
+          storageBucket: "authentication-6ccca.appspot.com",
+          messagingSenderId: "277980008767",
+          appId: "1:277980008767:web:73e31d14ceca8fc212038a",
+          measurementId: "G-VY6JZVT2FE"
+        };
+      
+        // Initialize Firebase
+        const app = initializeApp(firebaseConfig);
+        const database = getDatabase(app);
+        const auth = getAuth();
+      
+        const user = auth.currentUser;
+        onAuthStateChanged(auth, (user) => { // Get the currently signed-in user
+          if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          // here you can get currently signed in user with user object
+          // ...
+          } else {
+          // User is signed out
+          // ...
+         // if you want to make any changes after user logs in you can code it here
+          }
+        });
+      
+          logout.addEventListener('click',(e)=>{ // logging out a user 
+             signOut(auth).then(() => {
+            // Sign-out successful.
+            // -------------------------------------------------------
+            // Take user back to log in page, insert Code here
+            alert('user logged out');
+            }).catch((error) => {
+            // An error happened.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+      
+            alert(errorMessage);
+      });
+  });
